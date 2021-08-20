f48d61697040a4e15bd1328d518b36dd9e136269
Pablo Peralta <cellosofia1@gmail.com>
2021-08-20 10:46:25 -0400

    improvements into the install.sh script

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-cat bashrc >> ~/.bashrc
+#cat bashrc >> ~/.bashrc
+sudo cp bashrc /etc/profile.d/custom.sh
 cp tmux.conf ~/.tmux.conf
 cp vimrc ~/.vimrc
+
+source ~/.bashrc
+source /etc/profile.d/custom.sh
+
+
