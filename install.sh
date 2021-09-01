b1b1d09332e2edee73075e1123b5dda6b021db8a
Pablo Peralta <cellosofia1@gmail.com>
2021-09-01 16:27:28 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 cat bashrc >> ~/.bashrc
-cp tmux.conf ~/.tmux.conf
-cp vimrc ~/.vimrc
+cp -f tmux.conf ~/.tmux.conf
+cp -f vimrc ~/.vimrc
 
 source ~/.bashrc
-source /etc/profile.d/custom.sh
 
 
