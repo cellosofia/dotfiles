52a838bdee90def1f002dfd9e2447de9d924ba7e
Pablo Peralta <cellosofia1@gmail.com>
2026-02-24 11:02:27 -0300

    .

--- prepare_class.sh
+++ prepare_class.sh
@@ -5,13 +5,13 @@ source /etc/os-release
 
 VERSION="x"
 
-if [[ "${VERSION_ID}" =~ "^8.*" ]]; then
+if [[ "${VERSION_ID}" =~ ^8 ]]; then
     VERSION="8"
     sudo dnf install -y gnome-tweaks
-elif [[ "${VERSION_ID}" =~ "^9.*" ]]; then
+elif [[ "${VERSION_ID}" =~ ^9 ]]; then
     VERSION="9"
     sudo dnf install -y gnome-tweaks gnome-extensions-app
-elif [[ "${VERSION_ID}" =~ "^10.*" ]]; then
+elif [[ "${VERSION_ID}" =~ "^10" ]]; then
     VERSION="10"
     sudo dnf install -y gnome-tweaks gnome-extensions-app
 else
